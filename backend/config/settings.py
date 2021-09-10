8b02dc7f59fc64c6ec10a64027398f9526063cfd
Sana <smkhan_27@hotmail.com>
2021-09-10 00:32:55 -0700

    second commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -87,16 +87,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Heroku Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dch7n72frg031v',
-#         'USER': 'ldpelxievviuhf',
-#         'PORT': 5432,
-#         'HOST': 'ec2-23-23-164-251.compute-1.amazonaws.com',
-#         'PASSWORD': '8a9353d3c2aa7a6baaf128ed3c6fc0a4ede1cccfec1ccc0222e089e0871b8975',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd2fplodend039d',
+        'USER': 'gxojgcbtsfbcit',
+        'PORT': 5432,
+        'HOST': 'ec2-52-203-74-38.compute-1.amazonaws.com',
+        'PASSWORD': '29c38ba7d4524832caa34785d5d23a6c3762c4ed4a2ba74d738b82b98f4359b3',
+    }
+}
 
 # Local Database
 DATABASES = {
