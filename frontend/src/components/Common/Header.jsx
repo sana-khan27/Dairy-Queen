4da86a6de4deb83548800924a0558aa0ecc3a178
Sana <smkhan_27@hotmail.com>
2021-10-27 16:53:15 -0700

    update

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
             <div class="row">
                 <a href="#default" class="logo">DQ Dairy Queen</a>
             <div class="navbar-links">
-                <a class="sign-in" href="#">Sign in</a>
+                <a class="sign-in" href="/signin">Sign in</a>
                 <a href="#" >
                 <img src={ImgCart} /></a>
             </div>
